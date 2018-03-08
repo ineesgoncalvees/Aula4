b2d78bc94cb18c6504074cb266f6ae8a08de245e
ineesgoncalvees <ines98goncalves@gmail.com>
2018-03-08 10:32:08 +0000

    Criar metodo CountTo10

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,9 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyFirstMethod {
-    class Program {
-        static void Main(string[] args) {
+namespace MyAula04 {
+    class MyClass {
+
+        static void CountTo10() {
+            for (int i = 1; i <= 10; i++) ;
+            Console.WriteLine(i);
         }
+
+        static void Main(string[] args) {
+            
+        }
+
     }
 }
