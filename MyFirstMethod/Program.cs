3ca4dbe5f2eac550f11dfc5f65ce2a55c1812801
ineesgoncalvees <ines98goncalves@gmail.com>
2018-03-08 12:07:56 +0000

    Adicionar metodo Count

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -8,13 +8,23 @@ namespace MyAula04 {
     class MyClass {
 
         static void CountTo10() {
-            for (int i = 1; i <= 10; i++) ;
-            Console.WriteLine(i);
+            for (int i = 1; i <= 10; i++) {
+                Console.WriteLine(i);
+            }
+        }
+
+        static void CountToN(int n) {
+            for (int i = 1; i <= n; i++) {
+                Console.WriteLine(i);
+            }
         }
 
         static void Main(string[] args) {
             CountTo10();
             CountTo10();
+
+            CountToN(5);
+            CountToN(2);
         }
 
     }
